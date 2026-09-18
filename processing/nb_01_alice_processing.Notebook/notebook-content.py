ab39e123326ccf268f1c84b36fc38aa89cae3164
Michelle Santibañez Muñoz <92458075+misantibanez@users.noreply.github.com>
2026-09-18 10:52:22 -0400

    Committing 1 item from workspace f0028cdd-b92b-48bb-9996-4b38bf209fbf

--- processing/nb_01_alice_processing.Notebook/notebook-content.py
+++ processing/nb_01_alice_processing.Notebook/notebook-content.py
@@ -6,7 +6,19 @@
 # META   "kernel_info": {
 # META     "name": "synapse_pyspark"
 # META   },
-# META   "dependencies": {}
+# META   "dependencies": {
+# META     "lakehouse": {
+# META       "default_lakehouse": "0d261451-1f2c-80cb-438c-81257d4d0f82",
+# META       "default_lakehouse_name": "lk_01_processing",
+# META       "default_lakehouse_workspace_id": "00000000-0000-0000-0000-000000000000",
+# META       "known_lakehouses": [
+# META         {
+# META           "id": "0d261451-1f2c-80cb-438c-81257d4d0f82",
+# META           "workspace_id": "00000000-0000-0000-0000-000000000000"
+# META         }
+# META       ]
+# META     }
+# META   }
 # META }
 
 # CELL ********************
@@ -14,6 +26,17 @@
 # Welcome to your new notebook
 # Type here in the cell editor to add code!
 
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+1+1
 
 # METADATA ********************
 
